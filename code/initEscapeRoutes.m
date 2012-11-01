68128fa9180d85e498d54d89853081b6f11a2e13
Raphael Brechbuehler <raphabre@student.ethz.ch>
2012-11-01 20:31:21 +0100

    first simulation try with config1: it is possible to place the exit on the upper floors now. problem: agents do not move stairs up.

--- code/initEscapeRoutes.m
+++ code/initEscapeRoutes.m
@@ -6,12 +6,22 @@ for i=1:data.floor_count
 
     boundary_data = zeros(size(data.floor(i).img_wall));
     boundary_data(data.floor(i).img_wall) =  1;
-    %if (i == 1)
-        boundary_data(data.floor(i).img_exit) = -1;
-    %else
-        boundary_data(data.floor(i).img_stairs_down) = -1;
-    %end
+%     %if (i == 1)
+%         boundary_data(data.floor(i).img_exit) = -1;
+%     %else
+%         boundary_data(data.floor(i).img_stairs_down) = -1;
+%     %end
     
+if i<data.floor_exit
+    boundary_data(data.floor(i).img_stairs_up) = -1;
+    
+elseif i>data.floor_exit
+        boundary_data(data.floor(i).img_stairs_down) = -1;
+        
+    else 
+        boundary_data(data.floor(i).img_exit) = -1;
+        
+end
     exit_dist = fastSweeping(boundary_data) * data.meter_per_pixel;
     [data.floor(i).img_dir_x, data.floor(i).img_dir_y] = ...
         getNormalizedGradient(boundary_data, -exit_dist);
