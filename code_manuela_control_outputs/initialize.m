3845053c3bb96d748d1bf199336a98dc00cdcd8a
maeugste <maeugste@student.ethz.ch>
2012-12-06 19:37:13 +0100

    new code for control_outputs
    does not change anything for you guys. just be sure to have  the "code"
    folder as your matlab path  while you "compileC"

---
+++ code_manuela_control_outputs/initialize.m
@@ -0,0 +1,70 @@
+function data = initialize(config)
+% initialize the internal data from the config data
+%
+%  arguments:
+%   config      data structure from loadConfig()
+%
+%  return:
+%   data        data structure: all internal data used for the main loop
+%
+%               all internal data is stored in pixels NOT in meters
+
+
+data = config;
+
+%for convenience
+data.pixel_per_meter = 1/data.meter_per_pixel;
+
+fprintf('Init escape routes...\n');
+data = initEscapeRoutes(data);
+fprintf('Init wall forces...\n');
+data = initWallForces(data);
+fprintf('Init agents...\n');
+data = initAgents(data);
+
+% maximum influence of agents on each other
+
+data.r_influence = data.pixel_per_meter * ...
+    fzero(@(r) data.A * exp((2*data.r_max-r)/data.B) - 1e-4, data.r_max);
+
+fprintf('Init plots...\n');
+%init the plots
+%exit plot
+data.figure_exit=figure;
+hold on;
+axis([0 data.duration 0 data.total_agent_count]);
+title(sprintf('agents that reached the exit (total agents: %i)', data.total_agent_count));
+
+% floors plot
+data.figure_floors=figure;
+% figure('units','normalized','outerposition',[0 0 1 1])
+data.figure_floors_subplots_w = data.floor_count;
+data.figure_floors_subplots_h = 4;
+for i=1:config.floor_count
+        data.floor(i).agents_on_floor_plot = subplot(data.figure_floors_subplots_h, data.figure_floors_subplots_w, 3*data.floor_count - i+1 + data.figure_floors_subplots_w);
+        if i == config.floor_exit - 1
+            data.floor(i).building_plot = subplot(data.figure_floors_subplots_h, data.figure_floors_subplots_w, [(2*config.floor_count+1):3*config.floor_count]);
+        elseif i == config.floor_exit
+            data.floor(i).building_plot = subplot(data.figure_floors_subplots_h, data.figure_floors_subplots_w, [(config.floor_count+1):2*config.floor_count]);
+        elseif i == config.floor_exit + 1
+            data.floor(i).building_plot = subplot(data.figure_floors_subplots_h, data.figure_floors_subplots_w, [1:config.floor_count]);
+        end
+end
+
+% init output matrizes
+data.output = struct;
+data.output.config = config;
+data.output.agents_per_floor = ones(data.floor_count,data.duration/data.dt).*(-1);
+data.output.exit_left = zeros(data.exit_count,data.duration/data.dt);
+
+% prepare output file name
+data.output_file_name = ['output_' data.frame_basename];
+
+% prepare video file name
+data.video_file_name = ['video_' data.frame_basename '.avi'];
+
+% set deleted_agents to zero
+data.output.deleted_agents = 0;
+
+
+
---
+++ code_manuela_control_outputs/postprocessing.m
@@ -0,0 +1,161 @@
+% post processing of output.mat data from simulation
+% to run, you need to load the output first:
+% load('output_FILENAME');
+
+% tabula rasa
+close all
+clc
+
+% read in data from output
+agents_per_floor = output.agents_per_floor;
+config = output.config;
+exit_left = output.exit_left;
+simulation_time_real = output.simulation_time;
+dt = config.dt;
+
+
+% get users screen size
+screen_size = get(0, 'ScreenSize');
+
+% agents on boat
+agents_on_boat = sum(agents_per_floor(:,1:1:length(agents_per_floor)));
+
+% check if whole simulation was performed
+steps=config.duration/dt;
+for i=1:steps
+    if agents_on_boat(i)<0
+        steps=i-2;
+        break 
+    end
+end
+
+simulation_time_sim = steps*dt;
+
+% recalculate agents on boat
+agents_on_boat = sum(agents_per_floor(:,1:1:steps));
+agents_start = agents_on_boat(1);
+agents_left = agents_start-agents_on_boat;
+
+% find out t10, t50, t90, t100
+t10=0;
+for i=1:steps
+    if agents_left(i)<agents_start/10
+        t10=t10+dt;
+    end
+end
+if t10~=0
+    t10=t10+dt;
+end
+
+t50=0;
+for i=1:steps
+    if agents_left(i)<agents_start/2
+        t50=t50+dt;
+    end
+end
+if t50~=0
+    t50=t50+dt;
+end
+
+t90=0;
+for i=1:steps
+    if agents_left(i)<agents_start*0.9
+        t90=t90+dt;
+    end
+end
+if t90~=0
+    t90=t90+dt;
+end
+
+t100=0;
+if agents_left==agents_start
+    for i=1:steps
+        if agents_left(i)<agents_start
+            t100=t100+dt;
+        end
+    end
+end
+
+% create time axis
+if t100~=0
+    time = [0:dt:t100];
+else
+    time = [0:dt:simulation_time_sim];
+end
+steps = length(time);
+
+% recalculate agents on boat
+agents_on_boat = sum(agents_per_floor(:,1:1:steps));
+agents_start = agents_on_boat(1);
+agents_left = agents_start-agents_on_boat;
+agents_per_floor = agents_per_floor(:,1:1:steps);
+exit_left = exit_left(:,1:1:steps);
+
+% plot agents left over time
+f1 = figure;
+hold on
+grid on
+set(gca,'FontSize',16)
+plot(time,agents_left/agents_start*100,'LineWidth', 2)
+axis([0 time(end) 0 agents_left(end)/agents_start*100])
+title(sprintf('rescued agents (of total %i agents)',agents_start));
+xlabel('time [s]')
+ylabel('rescued agents out of all agents [%]')
+
+% plot agents_per_floor over time
+f2 = figure;
+hold on
+grid on
+set(gca,'FontSize',16)
+list = cell(config.floor_count,1);
+color = hsv(config.floor_count);
+color(config.floor_exit,:) = [0 0 0];
+for i=1:config.floor_count
+    plot(time,agents_per_floor(i,:),'LineWidth', 2,'color',color(i,:))
+    list{i} = [sprintf('floor %i',i)];
+end
+legend(list)
+
+axis([0 time(end) 0 max(max(agents_per_floor))+10])
+title(sprintf('agents per floor (of total %i agents)',agents_start));
+xlabel('time [s]')
+ylabel('agents per floor')
+
+% plot free places in rescue boats over time
+f3 = figure;
+hold on
+grid on
+set(gca,'FontSize',16)
+list = cell(config.exit_count,1);
+color = hsv(config.exit_count);
+for i=1:config.exit_count
+    plot(time,exit_left(i,:),'LineWidth', 2,'color',color(i,:))
+    list{i} = [sprintf('boat %i',i)];
+end
+legend(list)
+
+axis([0 time(end) 0 max(max(exit_left))+10])
+title('rescue boat capacity');
+xlabel('time [s]')
+ylabel('free places on rescue boat')
+
+% scale plots up to screen size
+set(f1, 'Position', [0 0 screen_size(3) screen_size(4) ] );
+set(f2, 'Position', [0 0 screen_size(3) screen_size(4) ] );
+set(f3, 'Position', [0 0 screen_size(3) screen_size(4) ] );
+
+
+% print out
+
+fprintf('Timestep: %f s\n', dt)
+fprintf('Steps simulated: %i\n', steps)
+fprintf('Simulation time: %f min\n', simulation_time_sim/60)
+fprintf('Agents on ship on start: %i\n', agents_start)
+fprintf('Agents on ship on simulation end: %i\n', agents_on_boat(end))
+fprintf('t_10: %f\n', t10)
+fprintf('t_50: %f\n', t50)
+fprintf('t_90: %f\n', t90)
+fprintf('t_100: %f\n', t100)
+
+
+
