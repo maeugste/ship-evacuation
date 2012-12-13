ba65c1ec886852b902535854f3659428bdd7d7d5
Raphael Brechbuehler <raphabre@student.ethz.ch>
2012-12-13 18:25:12 +0100

    doc crew command!!!

--- code_plots/compileC.m
+++
@@ -1,5 +0,0 @@
-mex 'fastSweeping.c'
-mex 'getNormalizedGradient.c'
-mex 'lerp2.c'
-mex 'createRangeTree.c'
-mex 'rangeQuery.c'
--- code_plots/plotExitedAgents.m
+++
@@ -1,6 +0,0 @@
-function plotExitedAgents(data)
-%plot time vs exited agents
-
-hold on;
-plot(data.time, data.agents_exited, 'r-');
-hold off;
