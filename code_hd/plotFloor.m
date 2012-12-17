7f1fe69ad444b725f7e4020776f720ed7ff6e67f
Raphael Brechbuehler <raphabre@student.ethz.ch>
2012-12-17 15:36:12 +0100

    code for high quality videos

---
+++ code_hd/plotFloor.m
@@ -0,0 +1,40 @@
+function plotFloor(data, floor_idx)
+
+if floor_idx == data.floor_exit-1 || floor_idx == data.floor_exit || floor_idx == data.floor_exit+1
+    h=subplot(data.floor(floor_idx).building_plot);
+       
+set(h, 'position',[0,0.35+0.65/3*(floor_idx-data.floor_exit+1),1,0.65/3-0.005]);
+
+hold off;
+% the building image
+imagesc(data.floor(floor_idx).img_plot);
+hold on;
+
+%plot options
+colormap(data.color_map);
+axis equal;
+axis manual; %do not change axis on window resize
+
+set(h, 'Visible', 'off')
+% title(sprintf('floor %i', floor_idx))
+
+text(10,240,sprintf('floor %i',floor_idx))
+if floor_idx == 3
+    text(10,120,sprintf('timestep: %0.5g',data.step-1),'color','white')
+    text(10,120,sprintf('timestep: %0.5g',data.step),'color','black')
+    text(10,150,sprintf('time: %0.5g s',data.time-data.dt),'color','white')
+    text(10,150,sprintf('time: %0.5g s',data.time),'color','black')
+end
+
+% plot agents
+if ~isempty(data.floor(floor_idx).agents)
+    ang = [linspace(0,2*pi, 10) nan]';
+    rmul = [cos(ang) sin(ang)] * data.pixel_per_meter;
+    draw = cell2mat(arrayfun(@(a) repmat(a.p,length(ang),1) + a.r*rmul, ...
+           data.floor(floor_idx).agents, 'UniformOutput', false)');
+    line(draw(:,2), draw(:,1), 'Color', 'r');
+end
+
+hold off;
+end
+end
